337fa4defcfe6caf978ffe62556ece07d5af6eb3
Sixela1702 <alexis.bonnetethgen@viacesi.fr>
2025-05-15 14:11:43 +0200

    ajout techlink

--- public/assets/js/ajouter.js
+++ public/assets/js/ajouter.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form");
 const titleInput = document.getElementById("title");
 const linkInput = document.getElementById("link");
+const techlinkInput = document.getElementById("techlink");
 const imageUrlInput = document.getElementById("image-url");
 const imageFileInput = document.getElementById("image-file");
 
@@ -9,15 +10,21 @@ form.addEventListener("submit", function (e) {
 
   const title = titleInput.value;
   const link = linkInput.value;
+  const techlink = techlinkInput.value;
   const imageUrl = imageUrlInput.value;
   const imageFile = imageFileInput.files[0];
 
+  if (!techlink) {
+    alert("Le lien technique est obligatoire.");
+    return;
+  }
+
   if (imageUrl) {
-    saveCard(title, link, imageUrl);
+    saveCard(title, link, techlink, imageUrl);
   } else if (imageFile) {
     const reader = new FileReader();
     reader.onload = function (event) {
-      saveCard(title, link, event.target.result);
+      saveCard(title, link, techlink, event.target.result);
     };
     reader.readAsDataURL(imageFile);
   } else {
@@ -27,10 +34,9 @@ form.addEventListener("submit", function (e) {
   form.reset();
 });
 
-function saveCard(title, link, imageSrc) {
-  const newCard = { title, link, imageSrc };
+function saveCard(title, link, techlink, imageSrc) {
+  const newCard = { title, link, techlink, imageSrc };
 
-  // Envoi vers Sheet.best
   fetch("https://6809e4101f1a52874cde3ce7.mockapi.io/documents", {
     method: "POST",
     headers: {
@@ -40,12 +46,12 @@ function saveCard(title, link, imageSrc) {
   })
     .then((res) => {
       if (!res.ok) {
-        throw new Error("Erreur lors de l'envoi vers Sheet.best");
+        throw new Error("Erreur lors de l'envoi vers MockAPI");
       }
       return res.json();
     })
     .then(() => {
-      alert("Document ajouté dans Google Sheet !");
+      alert("Document ajouté dans la base !");
     })
     .catch((error) => {
       console.error("Erreur :", error);
